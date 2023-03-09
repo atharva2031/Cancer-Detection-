a04d4864171d9d7316b5a3ee1c6ec78ab558e7bc
Atharva Dabeer <atharvad467@gmail.com>
2023-03-09 16:06:19 +0530

    Application Updated

---
+++ application/lib/pages/values.dart
@@ -0,0 +1,19 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class MyValuesPage extends StatefulWidget {
+  const MyValuesPage({super.key});
+
+  @override
+  State<MyValuesPage> createState() => _MyValuesPageState();
+}
+
+class _MyValuesPageState extends State<MyValuesPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
