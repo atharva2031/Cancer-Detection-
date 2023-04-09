12e8ae508b496f8f6c2f53f71771f43d2de42cfe
Atharva Dabeer <atharvad467@gmail.com>
2023-04-09 23:54:49 +0530

    Models added

---
+++ app/lib/pages/values.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MyValuesPage extends StatefulWidget {
+  const MyValuesPage({super.key});
+
+  @override
+  State<MyValuesPage> createState() => _MyValuesPageState();
+}
+
+class _MyValuesPageState extends State<MyValuesPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
